bfbb78545c51d431dbf11d4be590bb2b4034c4dc
artem bystrov <ockidocki1@gmail.com>
2023-04-09 19:54:47 +0400

    edits1

--- src/main/java/ru/netology/radioclass/Radio.java
+++ src/main/java/ru/netology/radioclass/Radio.java
@@ -10,5 +10,43 @@ import lombok.NoArgsConstructor;
 
 public class Radio {
 
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int minStation = 0;
+    private int maxStation = 9;
+    private int currentStation = minStation;
+    private int currentVolume = minVolume;
 
+    public Radio(int size) {
+        maxStation = minStation + size;
+    }
+
+    public void nextStation() {
+        int target = currentStation + 1;
+
+        if (target > maxStation) {
+            target = 0;
+        }
+        setCurrentStation(target);
+
+    }
+
+    public void prevStation() {
+        int target = currentStation - 1;
+
+        if (target < minStation) {
+            target = 9;
+        }
+        setCurrentStation(target);
+    }
+
+    public void increaseVolume() {
+        int target = currentVolume + 1;
+        setCurrentVolume(target);
+    }
+
+    public void decreaseVolume() {
+        int target = currentVolume - 1;
+        setCurrentVolume(target);
+    }
 }
